4c94cb41279255aea0532071218efdde6032ad49
Will Sams <will@samswebs.com>
2024-07-11 10:04:23 -0400

    Remove everest-dev-environment

--- everest-dev-environment/dependency_manager/setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
-"""Everest Dependency Manager."""
-
-from setuptools import setup, find_packages
-
-setup(
-    # see setup.cfg
-)
